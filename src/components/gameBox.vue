3abc703653b291507feb1c138e6f148e1ef77032
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2021-11-01 18:16:50 +0800

    基本界面

---
+++ src/components/gameBox.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="game-box">
+	<div class="item" v-for="i in 16" :key="i"></div>
+
+	<div class="el-box">
+		<slot></slot>
+	</div>
+</div>
+</template>
+<script>
+export default {
+	
+}
+</script>
+<style lang="scss" scope>
+.game-box{
+	box-sizing: border-box;
+	display: grid;
+	grid-template-rows: repeat(4, 80px);
+	grid-template-columns: repeat(4, 80px);
+	grid-gap: 10px 10px;
+	background: #bbada0;
+	border-radius: 10px;
+	padding: 10px;
+	width: fit-content;
+	margin: 0 auto;
+	position: relative;
+
+	.item{
+		background: #ccc0b3;
+		border-radius: 5px;
+	}
+
+	.el-box{
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+}
+</style>
---
+++ src/view/home.vue
@@ -0,0 +1,66 @@
+<template>
+<div class="container">
+
+	<!-- S 游戏信息 -->
+	<div class="msg-box">
+		<div class="game-name">2048-vue</div>
+		<!-- 新游戏按钮 -->
+		<div class="new-game">New Game</div>
+		<!-- 分数 -->
+		<div class="score">分数: {{score}}</div>
+	</div>
+	<!-- E 游戏信息 -->
+
+	<!-- S 游戏容器 -->
+	<game-box>
+
+	</game-box>
+	<!-- E 游戏容器 -->
+</div>
+</template>
+<script>
+import gameBox from '@/components/gameBox'
+export default {
+	data(){
+		return {
+			score: 0,
+		}
+	},
+  components: {
+		gameBox
+	},
+}
+</script>
+
+<style lang="scss" scope>
+.container{
+	margin: 0 auto;
+
+	.msg-box{
+		padding-top: 10vw;
+		text-align: center;
+
+		.game-name{
+			font-weight: bold;
+			font-size: 26px;
+		}
+
+		.new-game{
+			background: #8f7a66;
+			color: #fff;
+			width: 110px;
+			border-radius: 10px;
+			padding: 10px;
+			margin: 10px auto;
+		}
+
+		.score{
+			font-size: 18px;
+			margin-bottom: 30px;
+		}
+	}
+
+
+
+}
+</style>
